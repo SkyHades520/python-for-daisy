023a5d209f018d7fcf979dc8d9e29f8337ce618e
SkyHades520 <138434746+SkyHades520@users.noreply.github.com>
2023-07-03 14:44:09 +0800

    Add files via upload

---
+++ d.py
@@ -0,0 +1,63 @@
+#导入库
+from turtle import *
+
+screensize(400,300,"pink")
+t = Pen()
+t.pensize(3)
+t.hideturtle()
+
+'''
+⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻
+⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿
+⣿⣿⣿⡀⣿⣿⣿⣧⢻⣿⣶⣷⣿⣿⣿⣿⣿⣿⠿⠶⡝⠀⣿
+⣿⣿⣿⣷⠘⣿⣿⣿⢏⣿⣿⣋⣀⣈⣻⣿⣿⣷⣤⣤⣿⡐⢿
+⣿⣿⣿⣿⣆⢩⣝⣫⣾⣿⣿⣿⣿⡟⠿⠿⠦⠀⠸⠿⣻⣿⡄⢻
+⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⠇⣼
+⣿⣿⣿⣿⣿⣿⡄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰
+⣿⣿⣿⣿⣿⣿⠇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢀⣿
+⣿⣿⣿⣿⣿⠏⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿
+⣿⣿⣿⣿⠟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿
+⣿⣿⣿⠋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿
+⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸
+
+'''
+
+# 圈圈好难画。。。
+t.speed(1)
+t.color("black","#FFD91C")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+n = 20
+k = 360.0/n
+i = 0
+while i < 360:
+    #转向
+    t.penup()
+    t.speed(0)
+    t.home
+    t.goto(0,50)
+    t.setheading(0)
+    t.right(i)
+    t.fd(53)
+    i = i+k
+    t.pendown()
+    #转向要加速
+    t.speed(0)
+    t.right(5)
+
+    #花瓣
+    t.speed(1)
+    t.color("black","white")
+    t.begin_fill()
+    t.circle(500,18)
+    t.circle(20,180)
+    t.speed(0)
+    t.right(8)
+    t.speed(1)
+    t.circle(500,18)
+    t.end_fill()
+
+
+exitonclick() 
